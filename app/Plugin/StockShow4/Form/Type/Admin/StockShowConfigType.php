fc8412fbf0e8cc3ad99e6957872485595f8afe36
riki <r_karasawa@m.cajiya.co.jp>
2022-05-10 17:30:01 +0900

    StockShowConfigType.php雛形編集

--- app/Plugin/StockShow4/Form/Type/Admin/StockShowConfigType.php
+++ app/Plugin/StockShow4/Form/Type/Admin/StockShowConfigType.php
@@ -2,36 +2,31 @@
 
 namespace Plugin\StockShow4\Form\Type\Admin;
 
-use Plugin\StockShow4\Entity\Config;
+use Plugin\StockShow4\Entity\StockShowConfig;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
-class ConfigType extends AbstractType
+class StockShowConfigType extends AbstractType
 {
+    // フォーム項目を生成
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', TextType::class, [
-            'constraints' => [
-                new NotBlank(),
-                new Length(['max' => 255]),
-            ],
-        ]);
+        $builder->add('stock_qty_show', IntegerType::class);
     }
 
+    // data_classオプションでエンティティのクラス名を指定
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Config::class,
+            'data_class' => StockShowConfig::class,
         ]);
     }
 }
